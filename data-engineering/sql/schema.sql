1c6134eabb6f26a92911da736b9c5d3a8b71a97e
Hassan-Mef <156674177+Hassan-Mef@users.noreply.github.com>
2025-09-07 21:18:20 +0500

    Add initial SQL schema and dummy job data

---
+++ data-engineering/sql/schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS jobs (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(150),
+    company VARCHAR(150),
+    job_location VARCHAR(150),
+    job_type VARCHAR(150) 
+        DEFAULT 'Remote'
+        CHECK (job_type IN ('Remote' , 'Hybrid' , 'On-site')),
+    job_description TEXT,
+    skills TEXT[],
+    salary NUMERIC,                  -- optional (salary/budget)
+    experience_level VARCHAR(100),   -- optional
+    url TEXT UNIQUE,                 -- unique URL for the job post
+    created_at TIMESTAMP DEFAULT NOW()
+
+);\
+
+
+
+SELECT * FROM jobs;
+
+
+
